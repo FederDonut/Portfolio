bb707df9cf20aab7ef72e49f42a5b6d67774a70e
raphael <raphael.zwick8@gmail.com>
2025-11-19 06:27:29 +0100

    import techstack imgs

--- src/app/technologies/technologies.ts
+++ src/app/technologies/technologies.ts
@@ -9,4 +9,20 @@ import { CommonModule } from '@angular/common';
 })
 export class Technologies {
 
+  imgPath: string = "./../../public/assets/img/techImgs/Tech_icons"
+  
+  techStackImg = [
+    { titel: 'HTML', source: this.imgPath+'HTMl.png'},
+    { titel: 'CSS', source: this.imgPath+'CSS.png'},
+    { titel: 'JS', source: this.imgPath+'JS.png'},
+    { titel: 'Material Design', source: this.imgPath+'Material.png'},
+    { titel: 'TS', source: this.imgPath+'TS.png'},
+    { titel: 'Angular', source: this.imgPath+'Angular.png'},
+    { titel: 'Firebase', source: this.imgPath+'Firebase.png'},
+    { titel: 'GIT', source: this.imgPath+'GIT.png'},
+    { titel: 'API', source: this.imgPath+'API.png'},
+    { titel: 'Scrum', source: this.imgPath+'Scrum.png'},
+    { titel: 'Mindset', source: this.imgPath+'GrowthMindset.png'},
+  ]
 }
+
