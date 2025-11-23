3227cf6e6619f47327169fc8ed307db4295f003e
raphael <raphael.zwick8@gmail.com>
2025-11-23 12:49:34 +0100

    add hover functions

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
 
+  project: string|null  = null;
+
+  getBackgroundImage(){
+    if(this.project === 'Join'){
+      return 'url(/assets/img/Photos/Join-Photo.png)'
+    }
+    else if(this.project === 'El-Pollo-Loco'){
+      return 'url(/assets/img/Photos/Pollo-loco-Photo.png)'
+    }
+    else{
+      return null
+    }
+  }
+
+  identifyProject(projectTitel: string){
+    this.project = projectTitel;
+    console.log(this.project);
+  }
+
+  clearProjectName(){
+    this.project = null;
+  }
 }
