fad5f4ffdece46012f0bc80faf8c42144265461a
raphael <raphael.zwick8@gmail.com>
2025-11-29 16:47:23 +0100

    try another option of the slide

--- src/app/others-about-me/others-about-me.ts
+++ src/app/others-about-me/others-about-me.ts
@@ -1,16 +1,17 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-others-about-me',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './others-about-me.html',
   styleUrl: './others-about-me.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA], //Ignoriert unbekannte tags im HTML notwendig für swiper
 })
 export class OthersAboutMe {
 
+comments = [1,2,3]
 
   
 
