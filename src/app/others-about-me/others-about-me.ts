4c8a823873136ea7e1dcbea3b344e17a555cbfc1
raphael <raphael.zwick8@gmail.com>
2025-11-30 11:33:42 +0100

    add fkn slider with customized btn

--- src/app/others-about-me/others-about-me.ts
+++ src/app/others-about-me/others-about-me.ts
@@ -1,21 +1,63 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild, AfterViewInit,   } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-others-about-me',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    
+
+  ],
   templateUrl: './others-about-me.html',
   styleUrl: './others-about-me.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA], //Ignoriert unbekannte tags im HTML notwendig für swiper
 })
-export class OthersAboutMe {
+export class OthersAboutMe implements AfterViewInit {
 
-comments = [1,2,3]
+  @ViewChild('swiperContainer') swiperContainer: any;
+  
+  defaultPrevIconPath:string = '/assets/img/arrow_back.svg'
+  defaultNextIconPath:string = '/assets/img/arrow_forward.svg'
+  hoverPrevIconPath:string = '/assets/img/iconizer-arrow_back.svg'
+  hoverNextIconPath:string = '/assets/img/iconizer-arrow_forward.svg'
+
+  prevIcon = this.defaultPrevIconPath;
+  nextIcon = this.defaultNextIconPath;
+
+
+ 
+
+  ngAfterViewInit() {
+    this.setupSwiperNavigation();
+  }
+  
+  setupSwiperNavigation(){
+    //const swiperEl = this.swiperContainer.nativeElement;
+    const swiperEl = document.querySelector('swiper-container') as any;
+    const prevBtn = document.querySelector('.swiper-btn-prev') as HTMLButtonElement;
+    const nextBtn = document.querySelector('.swiper-btn-next') as HTMLButtonElement;
+
+    if (swiperEl && swiperEl.swiper) {
+
+      prevBtn?.removeEventListener('click', this.slidePrev);
+      nextBtn?.removeEventListener('click', this.slideNext);
+
+      prevBtn?.addEventListener('click', () => swiperEl.swiper.slidePrev());
+      nextBtn?.addEventListener('click', () => swiperEl.swiper.slideNext());
+    }
+  }
+  
+  slidePrev(){
+    this.slidePrev();
+  }
+
+  slideNext(){
+    this.slideNext();
+  }
+
 
   
-
-  
-
   
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { register as registerSwiperElements } from 'swiper/element/bundle'; 
 
+//import { register } from 'swiper/element/bundle';
+//// register Swiper custom elements
+//register()
+
 registerSwiperElements();
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
