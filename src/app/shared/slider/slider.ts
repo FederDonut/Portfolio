21658bac5fae1350ca0ef897bc941dd00baf4564
raphael <raphael.zwick8@gmail.com>
2025-12-06 09:47:37 +0100

    finish basic slider

--- src/app/shared/slider/slider.ts
+++ src/app/shared/slider/slider.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './slider.html',
   styleUrl: './slider.scss',
 })
-export class Slider implements AfterViewInit {
+export class Slider implements AfterViewInit, OnInit {
 
   // Referenz auf das scrollbare DIV-Element mit der Klasse .slider
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLDivElement>;
@@ -15,13 +15,59 @@ export class Slider implements AfterViewInit {
   comments = [0,1,2];
   mirrorComments:any = []; 
   firstCurrentSlideIndex:number = 1
+  currentSlideIndex: number = 0
+
+  //Test
+  currentCommentId: any = this.comments[0];
+
+  defaultPrevIconPath:string = '/assets/img/arrow_back.svg'
+  defaultNextIconPath:string = '/assets/img/arrow_forward.svg'
+  hoverPrevIconPath:string = '/assets/img/iconizer-arrow_back.svg'
+  hoverNextIconPath:string = '/assets/img/iconizer-arrow_forward.svg'
+
+  prevIcon = this.defaultPrevIconPath;
+  nextIcon = this.defaultNextIconPath;
 
   ngOnInit(){
     this.storeMirror();
+    this.currentSlideIndex = 0;
+  }
+
+  updateActivePagination(){
+    
+    const sliderEl = this.sliderRef.nativeElement;
+    const commentElement = sliderEl.querySelector('.comment');
+    const slideWidth = commentElement?.clientWidth || 0;
+    const gap = 32;
+    const slideSize = slideWidth + gap;
+    //const targetIndex = index +1;
+    const scrollLeft = sliderEl.scrollLeft;
+    
+    // Berechne den Index im mirrorComments Array (0, 1, 2, 3, 4)
+    const mirrorIndex = Math.round(scrollLeft / slideSize);
+
+    // Konvertiere zum Index des Original-Arrays (0, 1, 2)
+    let newOriginalIndex = mirrorIndex - 1;
+
+    // Behandlung des Looping-Jumps (Index-Anpassung)
+    if (newOriginalIndex < 0) {
+        // Linker Spiegel-Slide -> wird als letzter echter Slide betrachtet
+        newOriginalIndex = this.comments.length - 1; 
+    } else if (newOriginalIndex >= this.comments.length) {
+        // Rechter Spiegel-Slide -> wird als erster echter Slide betrachtet
+        newOriginalIndex = 0;
+    }
+
+    // Setze die ID des Kommentars, die nun im Pagination-HTML verglichen wird
+    this.currentCommentId = this.comments[newOriginalIndex];
+    
   }
 
   ngAfterViewInit(): void {
-      this.startPosition(this.firstCurrentSlideIndex,'auto');
+     this.startPosition(this.firstCurrentSlideIndex,'auto');
+    setTimeout(() => {
+        this.updateActivePagination();
+    }, 0);
   }
 
   storeMirror(){
@@ -48,14 +94,16 @@ export class Slider implements AfterViewInit {
     if (direction === 'next' && currentIndex === lastCurrentSlidendex) {
         setTimeout(() => {
             this.startPosition(this.firstCurrentSlideIndex, 'auto');
+            this.updateActivePagination()
         }, 300); 
     }
     if (direction === 'prev' && currentIndex === this.firstCurrentSlideIndex) {
         setTimeout(() => {
             this.startPosition(lastCurrentSlidendex, 'auto');
+            this.updateActivePagination()
         }, 300); 
     }
-    
+    //this.changePagination(currentIndex) 
   }
 
   startPosition(index:number, behavior: 'auto'|'smooth'){
@@ -92,11 +140,11 @@ export class Slider implements AfterViewInit {
     sliderEl.scrollBy({ left: scrollByAmount, behavior: 'smooth' });
   }
 
-  slideNext(){
-    this.scroll('next');
-  }
-
   slidePrev(){
     this.scroll('prev');
+  }
+
+  slideNext(){
+    this.scroll('next');
   }
 }
