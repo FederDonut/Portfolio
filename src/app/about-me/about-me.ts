ad94e9527e97101ed0665438312853dbd6aa6ef0
raphael <raphael.zwick8@gmail.com>
2026-01-17 12:20:12 +0100

    update overlay responsivnes & add some animations

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 
 @Component({
@@ -9,6 +9,27 @@ import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
-export class AboutMe {
+export class AboutMe implements AfterViewInit {
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const options = {
+      root: null, 
+      threshold: 0.2 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(entry.target, 'visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+    const photo = this.el.nativeElement.querySelector('.photo-wrapper');
+    const description = this.el.nativeElement.querySelector('.description-wrapper');
+    if (photo) observer.observe(photo);
+    if (description) observer.observe(description);
+  }
 }
--- src/app/technologies/technologies.ts
+++ src/app/technologies/technologies.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit, ElementRef, Renderer2  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgStyle, NgIf } from '@angular/common';
 import { TechOverlay } from '../overlays/tech-overlay/tech-overlay';
@@ -16,8 +16,9 @@ import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
   templateUrl: './technologies.html',
   styleUrl: './technologies.scss',
 })
-export class Technologies {
+export class Technologies implements AfterViewInit{
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
   imgPath: string = "assets/img/techImgs/Tech_icons"
   showTechOverlay : boolean = false;
   
@@ -48,6 +49,29 @@ export class Technologies {
     this.showTechOverlay = false;
   }
 
+
+    
+
+  ngAfterViewInit() {
+    const options = {
+      root: null, 
+      threshold: 0.2 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(entry.target, 'visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+    const photo = this.el.nativeElement.querySelector('.skill-set-wrapper');
+    const description = this.el.nativeElement.querySelector('.skill-imgs-wrapper');
+    if (photo) observer.observe(photo);
+    if (description) observer.observe(description);
+  }
+  
 
 }
 
